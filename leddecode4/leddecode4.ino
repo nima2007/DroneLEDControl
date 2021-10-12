b57f7de381514eef57dfc6dd1f1e3c3dde83ae38
Nima <v.nima2007@gmail.com>
2021-10-11 21:59:50 -0700

    all working

--- leddecode4/leddecode4.ino
+++ leddecode4/leddecode4.ino
@@ -1,4 +1,7 @@
 
+//#include <Servo.h> 
+#include <PWMServo.h>
+
 
 #define NOP3 "nop\n\t""nop\n\t""nop\n\t"
 #define NOP4 "nop\n\t""nop\n\t""nop\n\t""nop\n\t"
@@ -7,11 +10,14 @@
 
 #define signalpin 3
 #define oscpin 11
+#define servopin 10
 
 unsigned char test = 0;
 unsigned char res = 0;
+PWMServo myservo;
+bool is_open = false;
 
-enum color
+enum DroneColor
 {
   grey = 194,
   orange = 80, 
@@ -30,18 +36,28 @@ void setup() {
   pinMode(oscpin, OUTPUT);
   digitalWrite(oscpin, LOW);
   delayMicroseconds(1000);
+  myservo.attach(servopin);
+  motor_close(true);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  res = get_color();
-  if(res == 80)
-  {
-    digitalWrite(oscpin, HIGH);
-  }
+  
   delay(2000);
+  res = get_color();
+  if(res == DroneColor::orange)
+  {
+    //digitalWrite(oscpin, HIGH);
+    motor_open();
+    delay(1000);
+  }
+  else if(res == DroneColor::purple)
+  {
+    motor_close();
+    delay(1000);
+  }
+  
   
 }
 
@@ -83,82 +99,51 @@ unsigned char read_packet(void)
   }
 
   while(digitalReadFast(signalpin) == LOW){} // Start of data
-  //__asm(NOP6);
 
   for(int i = 0;i<8;i++) // Read 8 bits
   {
-    __asm(NOP6);
+    __asm(NOP6); // offset from start of high to measauring point
     __asm(NOP6);
     __asm(NOP6);
     __asm(NOP6);
     __asm(NOP6);
     digitalWriteFast(oscpin, HIGH);
-    //__asm(NOP6);
     bitWrite(test, i, digitalReadFast(signalpin) == HIGH);
     digitalWriteFast(oscpin, LOW);
-    /*__asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP6);
-    __asm(NOP4);*/
-    while(digitalReadFast(signalpin) == HIGH){}
-    while(digitalReadFast(signalpin) == LOW){}
+    while(digitalReadFast(signalpin) == HIGH){} // wait for high to stop
+    while(digitalReadFast(signalpin) == LOW){} // wait for new high
 
   }
 
-  /*digitalWriteFast(oscpin, HIGH);
-  //__asm(NOP6);
-  bitWrite(test, 1, digitalReadFast(oscpin));
-  //__asm( "nop" );
-  digitalWriteFast(oscpin, LOW);
-  __asm(NOP6);
-
-  /*__asm( "nop" );
-  __asm( "nop" );
-  __asm( "nop" );
-  __asm( "nop" );
-  __asm( "nop" );
-  __asm( "nop" );
-  __asm( "nop" );
-  __asm( "nop" );
-  __asm( "nop" );
-  __asm( "nop" );*/
- /*
-  digitalWriteFast(oscpin, LOW);
-  test = digitalReadFast(signalpin);
-
-  if(test == 0)
-  {
-    __asm(NOP6);
-    digitalWriteFast(oscpin, HIGH);
-    __asm(NOP6);
-    digitalWriteFast(oscpin, LOW);
-  }
-  */
-  /*for (int j = 0;j < test; j++)
-  {
-    digitalWriteFast(oscpin, HIGH);
-    __asm(NOP6);
-    digitalWriteFast(oscpin, LOW);
-    __asm(NOP6);
-    
-  }*/
   interrupts()
   bitWrite(test, 0, 0); // ignore first captured bit since timing is bad
   return test;
+  
+}
+
+void motor_open()
+{
+  if(is_open)
+  {
+    return;
+  }
+  myservo.write(130);
+  is_open = true;
+}
+
+void motor_close(bool force)
+{
+  if(!is_open && !force)
+  {
+    return;
+  }
+  myservo.write(170);
+  is_open = false;
+  
+}
+
+void motor_close()
+{
+  return motor_close(false);
   
 }
